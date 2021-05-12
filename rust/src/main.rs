59235d53888b823165566ba5d6b2e17f1d6ab26b
Milad Rastian <milad@high-mobility.com>
2021-05-13 01:12:45 +0200

    async fetch

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,9 @@
 use futures::executor::block_on;
+use futures::future::join_all;
 use reqwest::header::USER_AGENT;
 use reqwest::Error;
 use serde::Deserialize;
+use std::time::SystemTime;
 
 #[derive(Deserialize, Debug)]
 struct User {
@@ -15,7 +17,6 @@ async fn fetch_users() -> Result<Vec<User>, Error> {
         owner = "rust-lang-nursery",
         repo = "rust-cookbook"
     );
-    println!("{}", request_url);
     let client = reqwest::Client::new();
     let response = client
         .get(&request_url)
@@ -23,20 +24,38 @@ async fn fetch_users() -> Result<Vec<User>, Error> {
         .send()
         .await?;
     let users: Vec<User> = response.json().await?;
-    return Ok(users);
+    Ok(users)
+}
+
+async fn fetch_user(user_struct: &User) -> Result<User, Error> {
+    let request_url = format!(
+        "https://api.github.com/users/{user}",
+        user = user_struct.login
+    );
+    let now = SystemTime::now();
+    println!("fetching user: {:?} @ {:?}", request_url, now);
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&request_url)
+        .header(USER_AGENT, "My Rust Program 1.0")
+        .send()
+        .await?;
+        let user: User = response.json().await?;
+        Ok(user)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     match block_on(fetch_users()) {
         Ok(users) => {
-            let filtered_users = users.iter().filter(|u| u.login.starts_with("s"));
-            println!("{:?}", filtered_users);
+            let filtered_users = users
+            .iter()
+            .filter(|u| u.login.starts_with("s"))
+            .map(|u| fetch_user(u));
+            let fetch_all_users = join_all(filtered_users).await;
+            println!("{:?}", fetch_all_users)
         }
         Err(error) => println!("Oops {:?}", error),
     }
-
-    /*
-     */
     Ok(())
 }
